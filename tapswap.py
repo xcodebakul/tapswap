39d2d7be98ce0536db0ccb7480988df3e3c7b27a
MidJune Basecamp <gudangsembako59@gmail.com>
2024-06-16 15:34:17 +0700

    Update tapswap.py

--- tapswap.py
+++ tapswap.py
@@ -194,6 +194,7 @@ else:
 # Kemudian, modifikasi fungsi `submit_taps` untuk memperhitungkan input ini
 def submit_taps(access_token, energy, boost_ready, energy_ready, content_id, time_stamp, init_data_line):
     global turbo_activated
+    tap_count = 0
 
     while True:
         url = "https://api.tapswap.ai/api/player/submit_taps"
@@ -246,16 +247,17 @@ def submit_taps(access_token, energy, boost_ready, energy_ready, content_id, tim
         }
 
         if turbo_activated == True:
-            total_taps = 1000000
+            total_taps = random.randint(20, 30)
             payload = {"taps": total_taps, "time": int(time_stamp)}
         else:
-            total_taps = 10000
+            total_taps = random.randint(20, 30)
             payload = {"taps": total_taps, "time": int(time_stamp)}
 
        
 
         if turbo_activated == True:
-            for _ in range(30):
+            for _ in range(20):
+                time.sleep(2)
                 response = requests.post(url, headers=headers, json=payload)
                 if response.status_code == 201:
                     print(f"\r{Fore.GREEN+Style.BRIGHT}[ Tap ] : Tapped              ", flush=True)
@@ -263,13 +265,17 @@ def submit_taps(access_token, energy, boost_ready, energy_ready, content_id, tim
                     print(f"\r{Fore.RED+Style.BRIGHT}[ Tap ] : Gagal mengirim taps, status code: {response.status_code}")
             turbo_activated = False
         else:
+            time.sleep(1)
             response = requests.post(url, headers=headers, json=payload)
             if response.status_code == 201:
                 print(f"\r{Fore.GREEN+Style.BRIGHT}[ Tap ] : Tapped            ", flush=True)
-
+                tap_count += 1
+                if tap_count == 20:
+                    print(f"\r{Fore.YELLOW+Style.BRIGHT}[ Tap ] : Tapped 20x, ganti akun", flush=True)
+                    return
                 if use_upgrade == 'y' :
-                    upgrade_level(headers, "tap")
-                    upgrade_level(headers, "energy")
+                    # upgrade_level(headers, "tap")
+                    # upgrade_level(headers, "energy")
                     upgrade_level(headers, "charge")
                 cek_energy = response.json().get("player").get("energy")
                 if cek_energy < 50:
